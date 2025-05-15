12a81a5417e1ad1c67cccfc2b5e9500d57000d21
Sebastian Joseph <sebyjoseph2015@gmail.com>
2025-05-15 17:58:09 -0400

    reqs_resolve resolved

--- reqs_resolve/install_packages.sh
+++ reqs_resolve/install_packages.sh
@@ -1,3 +1,3 @@
 pip install astropy-iers-data
 pip install certifi
-grep -Ev '^[[:space:]]*#|^[[:space:]]*$' requirements_v2.txt | xargs -n1 -I{} pip install {} --no-deps
+grep -Ev '^[[:space:]]*#|^[[:space:]]*$' requirements.txt | xargs -n1 -I{} pip install {} --no-deps
