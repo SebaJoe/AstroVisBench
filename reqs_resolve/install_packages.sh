7908fc29089663c663e91cc29e172df3353b0857
Sebastian Joseph <sebyjoseph2015@gmail.com>
2025-05-18 17:24:38 -0500

    small update to install_pkgs

--- reqs_resolve/install_packages.sh
+++ reqs_resolve/install_packages.sh
@@ -1,3 +1,5 @@
 pip install astropy-iers-data
 pip install certifi
+pip install mpi4py
+pip install importlib
 grep -Ev '^[[:space:]]*#|^[[:space:]]*$' requirements.txt | xargs -n1 -I{} pip install {} --no-deps
